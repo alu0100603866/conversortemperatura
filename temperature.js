b8478782ef9f2557386cbe138cafdc71c64aa777
Juan Tareq Gonzalez <alu0100603866@ull.edu.es>
2014-01-29 15:40:56 +0000

    Practica realizada

--- temperature.js
+++ temperature.js
@@ -4,7 +4,7 @@ function calculate() {
   var result;
   var original = document.getElementById('original');
   var temp = original.value;
-  var regexp = /([+-]?\d+(?:\.\d+)?)\s?([cCfF])/;
+  var regexp = /([+-]?\d+(?:\.\d+)?)\s*([cCfF])/;
   
   var m = temp.match(regexp);
   
